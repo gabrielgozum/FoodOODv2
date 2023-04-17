ff89b81c98a7799fb4258dc0189f2d0b04523306
yuxianshi <7328542+yuxianshi@users.noreply.github.com>
2023-04-17 18:22:12 -0500

    Update separate_yelp_photos.sh

--- Separate_Yelp_photos/separate_yelp_photos.sh
+++ Separate_Yelp_photos/separate_yelp_photos.sh
@@ -3,6 +3,8 @@ Place food_no_101label_words_list.txt, food_food101caption_list.txt,
 food_nocaption_list.txt, non_food_list.txt in the folder that has the 
 Yelp photos, cd to this folder and run the cmds below to copy (or move) 
 images to separate folders.
+
+This script works on Mac in Terminal. If use Linux, may try using 'cat non_food_list.txt | xargs -I % echo cp % non_food' instead.
 '
 
 mkdir food_no_101label_words
