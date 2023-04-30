cb5c9281726f348b0d382448277ea81de426044a
yuxianshi <7328542+yuxianshi@users.noreply.github.com>
2023-04-30 15:04:30 -0500

    Update separate_yelp_photos.sh

--- Separate_Yelp_photos/separate_yelp_photos.sh
+++ Separate_Yelp_photos/separate_yelp_photos.sh
@@ -12,7 +12,14 @@ mkdir food_food101caption
 mkdir food_nocaption
 mkdir non_food
 
+## on mac os
 cat food_no_101label_words_list.txt | xargs -J % cp % food_no_101label_words # you may replace 'cp' by 'mv'
 cat food_food101caption_list.txt | xargs -J % cp % food_food101caption # you may replace 'cp' by 'mv'
 cat food_nocaption_list.txt | xargs -J % cp % food_nocaption # you may replace 'cp' by 'mv'
 cat non_food_list.txt | xargs -J % cp % non_food # you may replace 'cp' by 'mv'
+
+## on linux
+for file in $(<food_no_101label_words_list.txt); do cp -- "$file" food_no_101label_words; done
+for file in $(<food_food101caption_list.txt); do cp -- "$file" food_food101caption; done
+for file in $(<food_nocaption_list.txt); do cp -- "$file" food_nocaption; done
+for file in $(<non_food_list.txt); do cp -- "$file" non_food; done
